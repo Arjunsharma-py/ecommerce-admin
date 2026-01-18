681f8ac6a83cf9118ecb3b43eadcbfb2f2d27fe5
Arjunsharma-py <arjunsharma5330@gmail.com>
2026-01-18 13:10:59 +0530

    CI/CD: testing

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   // 👇 Important for GitHub Pages
-  base: '/ecommerce-admin/',   // replace with your repo name
+  // base: '/ecommerce-admin/',   // replace with your repo name
   server: {
     port: 3000,
     proxy: {
-      '/api': {
-        target: 'https://ecommerce-api.urgentprinters.com',
+      "/api": {
+        target: "https://ecommerce-api.urgentprinters.com",
         changeOrigin: true,
-      }
-    }
-  }
-})
+      },
+    },
+  },
+});
